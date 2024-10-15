1f123538d94aebeb477bbcaa7f567c231943ccca
Vladimir Alfaro <vladimir.ac29@gmail.com>
2024-10-14 22:48:12 -0400

    feat: created stepper component for multi-step form

---
+++ src/components/Stepper.tsx
@@ -0,0 +1,52 @@
+"use client";
+import * as React from "react";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  type CarouselApi,
+} from "@/components/ui/carousel";
+
+export default function Stepper() {
+  const [api, setApi] = React.useState<CarouselApi>();
+  const [current, setCurrent] = React.useState(0);
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
+  return (
+    <div className="h-11/12 w-11/12">
+      <Carousel setApi={setApi} className="w-full h-full">
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <Card>
+                <CardContent className="flex h-screen items-center justify-center p-6">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        {/* <CarouselPrevious />
+        <CarouselNext /> */}
+      </Carousel>
+      {/* <div className="py-2 text-center text-sm text-muted-foreground">
+        Slide {current} of {count}
+      </div> */}
+    </div>
+  );
+}
