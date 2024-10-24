b63c6d3ba300bcbe18528a43bc46ba9aefadb251
Vladimir Alfaro <vladimir.ac29@gmail.com>
2024-10-23 23:07:36 -0400

    feat: added route handle to retrieve data from mistral API

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,25 @@
+import { createMistral } from "@ai-sdk/mistral";
+import { convertToCoreMessages, streamText } from "ai";
+
+const mistral = createMistral({
+  baseURL: "https://api.mistral.ai/v1",
+  apiKey: process.env.MISTRAL_API_KEY,
+});
+
+//Options: "mistral-large-latest","mistral-small-latest","ministral-3b-latest","ministral-8b-latest","pixtral-12b-2409"
+const model = mistral("pixtral-12b-2409", {
+  safePrompt: false, // optional safety prompt injection
+});
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const result = await streamText({
+    model: model,
+    messages: convertToCoreMessages(messages),
+  });
+
+  return result.toDataStreamResponse();
+}
