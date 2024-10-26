1a795c94f4247b3200b240b8f14d4cb4859338d6
Vladimir Alfaro <vladimir.ac29@gmail.com>
2024-10-26 14:03:19 -0400

    chore: styled model card component to improve UI

--- src/components/ModelCard.tsx
+++ src/components/ModelCard.tsx
@@ -1,6 +1,11 @@
 "use client";
 
-import { Card } from "./ui/card";
+import { Card } from "@/components/ui/card";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 type modelItem = {
   id: number | string;
@@ -18,12 +23,19 @@ export default function ModelCard({
   const hoverAnimation =
     "shadow-[0_5px_0_rgb(0,0,0)] hover:shadow-[0_2px_0px_rgb(0,0,0)] hover:cursor-pointer ease-out hover:translate-y-1 transition-all";
   return (
-    <Card
-      key={id}
-      className={`realtive h-20 w-20 flex flex-col p-4 items-center justify-center text-center overflow-hidden ${isSelected ? "animate-in duration-100 border-2 border-black " : "animate-out duration-100"} ${hoverAnimation}`}
-      onClick={() => onClick()}
-    >
-      <h1 className="">{name}</h1>
-    </Card>
+    <Tooltip delayDuration={500}>
+      <TooltipTrigger className="cursor-pointer" asChild>
+        <Card
+          key={id}
+          className={`realtive h-auto w-auto flex flex-col p-4 items-center justify-center text-center text-wrap overflow-x-hidden ${isSelected ? "animate-in duration-100 border-2 border-black " : "animate-out duration-100"} ${hoverAnimation}`}
+          onClick={() => onClick()}
+        >
+          {name}
+        </Card>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>{name}</p>
+      </TooltipContent>
+    </Tooltip>
   );
 }
