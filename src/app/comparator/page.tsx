ebdef1f9e4c37bd42c708078e3e68b64460c3c6a
Vladimir Alfaro <vladimir.ac29@gmail.com>
2024-10-25 23:23:23 -0400

    feat: fixe warning passing functions as props

--- src/app/comparator/page.tsx
+++ src/app/comparator/page.tsx
@@ -2,19 +2,38 @@
 
 import ChatBody from "@/components/chat/ChatBody";
 import { useChat } from "ai/react";
+import { useCallback } from "react";
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat({
     api: "/api/mistralChat",
   });
+
+  // Memoize functions to render a new function when its props changes only
+  const memoizedHandleInputChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      handleInputChange(e);
+    },
+    [handleInputChange],
+  );
+
+  const memoizedHandleSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      handleSubmit(e);
+    },
+    [handleSubmit],
+  );
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      <ChatBody
-        messages={messages}
-        input={input}
-        handleInputChange={handleInputChange}
-        handleSubmit={handleSubmit}
-      />
+    <div className="flex flex-wrap w-full">
+      {[0, 1, 2, 3].map((c) => (
+        <ChatBody
+          key={c}
+          messages={messages}
+          input={input}
+          handleInputChange={memoizedHandleInputChange}
+          handleSubmit={memoizedHandleSubmit}
+        />
+      ))}
     </div>
   );
 }
