c3726016a3cad61b2813c12d2777beeec85db9f2
Vladimir Alfaro <vladimir.ac29@gmail.com>
2024-10-27 18:27:27 -0400

    feat: added removeModel state function

--- src/stores/ModelsStore.tsx
+++ src/stores/ModelsStore.tsx
@@ -3,6 +3,12 @@ import { create } from "zustand";
 
 export const modelsStore = create((set) => ({
   models: [],
+  removeModel: (selectedModel: modelItem) =>
+    set((state) => ({
+      models: state.models.filter(
+        (model: modelItem) => selectedModel.id !== model.id,
+      ),
+    })),
   updateModels: (selectedModels: modelItem[]) =>
     set({ models: selectedModels }),
 }));
