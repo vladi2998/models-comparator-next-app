b455f679672e3b744e905155f2fbeebd2e91ab7f
Vladimir Alfaro <vladimir.ac29@gmail.com>
2024-10-27 20:28:35 -0400

    feat: added disable prop for unavailable models

--- src/components/ModelCard.tsx
+++ src/components/ModelCard.tsx
@@ -12,6 +12,7 @@ type modelItem = {
   name: string;
   isSelected: boolean;
   onClick: () => void;
+  disable: boolean;
 };
 
 export default function ModelCard({
@@ -19,22 +20,31 @@ export default function ModelCard({
   name,
   isSelected,
   onClick,
+  disable = false,
 }: modelItem) {
   const hoverAnimation =
     "shadow-[0_5px_0_rgb(0,0,0)] hover:shadow-[0_2px_0px_rgb(0,0,0)] hover:cursor-pointer ease-out hover:translate-y-1 transition-all";
+
+  const handleClick = () => {
+    if (disable) return;
+    onClick();
+  };
   return (
     <Tooltip delayDuration={500}>
       <TooltipTrigger className="cursor-pointer" asChild>
         <Card
           key={id}
-          className={`realtive h-auto w-auto flex flex-col p-4 items-center justify-center text-center text-wrap overflow-x-hidden ${isSelected ? "animate-in duration-100 border-2 border-black " : "animate-out duration-100"} ${hoverAnimation}`}
-          onClick={() => onClick()}
+          className={`realtive h-auto w-auto flex flex-col p-4 items-center justify-center text-center text-wrap overflow-x-hidden ${isSelected ? "animate-in duration-100 border-2 border-black " : "animate-out duration-100"} ${disable ? "bg-primary-foreground bg-opacity-50 text-slate-500" : hoverAnimation}`}
+          onClick={handleClick}
         >
           {name}
         </Card>
       </TooltipTrigger>
       <TooltipContent>
-        <p>{name}</p>
+        <p>
+          {name}
+          {disable && ":  not available"}
+        </p>
       </TooltipContent>
     </Tooltip>
   );
